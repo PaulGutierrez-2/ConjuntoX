7a5945ab3a20bb19ea89802fec036299ec060e8e
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-05 07:46:57 -0500

    Se agrego la pantalla de Info

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
