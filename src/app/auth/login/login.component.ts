2111691898d32013b820f23206f6de4c514ad97d
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-04 22:51:31 -0500

    Avance, Se agrego las rutas con routerLink para navegar a otra ruta y se creo un componente boton

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
