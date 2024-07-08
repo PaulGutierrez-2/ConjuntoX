daaa0b19cd464378cb2f5ed61c500a349cf51b1c
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-08 10:17:32 -0500

    se creo un nuevo componente TextBox

--- src/app/pages/learning-path/learning-path.component.ts
+++ src/app/pages/learning-path/learning-path.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TextBoxComponent } from '../../components/text-box/text-box.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-learning-path',
   standalone: true,
-  imports: [],
+  imports: [TextBoxComponent, RouterLink],
   templateUrl: './learning-path.component.html',
   styleUrl: './learning-path.component.css'
 })
