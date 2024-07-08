39eee3592607388184464826174b892611e9007d
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-08 07:34:53 -0500

    nueva ruta 'learning path' en routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { InfoComponent } from './pages/info/info.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LearningPathComponent } from './pages/learning-path/learning-path.component';
 
 export const routes: Routes = [
+    {path:'', component: HomeComponent},
     {path:'login', component: LoginComponent},
     {path:'register', component: RegisterComponent},
     {path:'info', component: InfoComponent},
+    {path:'learningpath', component: LearningPathComponent},
 ];
