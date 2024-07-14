275a740c1f482e8e217ef2b9c4cc2e3b90611448
Angel Zuniga <mazuniga@sudamericano.edu.ec>
2024-07-14 17:18:33 -0500

    creacion de paginas de cada nivel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,24 @@ import { RegisterComponent } from './auth/register/register.component';
 import { InfoComponent } from './pages/info/info.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LearningPathComponent } from './pages/learning-path/learning-path.component';
-
+import { SeTheoryComponent } from './pages/topics/se-theory/se-theory.component';
+import { UnionComponent } from './pages/topics/union/union.component';
+import { CardinalityComponent } from './pages/topics/cardinality/cardinality.component';
+import { IntersectionComponent } from './pages/topics/intersection/intersection.component';
+import { DifferenceComponent } from './pages/topics/difference/difference.component';
+import { ComplementComponent } from './pages/topics/complement/complement.component';
+import { SymmetricDifferenceComponent } from './pages/topics/symmetric-difference/symmetric-difference.component';
 export const routes: Routes = [
     {path:'', component: HomeComponent},
     {path:'login', component: LoginComponent},
     {path:'register', component: RegisterComponent},
     {path:'info', component: InfoComponent},
     {path:'learningpath', component: LearningPathComponent},
+    {path:'setheory', component: SeTheoryComponent},
+    {path:'cardinality', component:CardinalityComponent},
+    {path:'union', component: UnionComponent},
+    {path:'intersection', component: IntersectionComponent},
+    {path:'difference', component: DifferenceComponent},
+    {path:'complement', component: ComplementComponent},
+    {path:'SymDifference', component: SymmetricDifferenceComponent},
 ];
--- src/app/pages/topics/se-theory/se-theory.component.ts
+++ src/app/pages/topics/se-theory/se-theory.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-se-theory',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './se-theory.component.html',
   styleUrl: './se-theory.component.css'
 })
