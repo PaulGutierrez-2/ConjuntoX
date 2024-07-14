f8d70b2511e80946991a392f92f2dc57717b02ca
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-14 01:17:01 -0500

    Se creo la pagina teoria de conjuntos html, css, ts

--- src/app/pages/topics/se-theory/se-theory.component.ts
+++ src/app/pages/topics/se-theory/se-theory.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { TopicStartComponent } from '../../../components/topic-start/topic-start.component';
 
 @Component({
   selector: 'app-se-theory',
   standalone: true,
-  imports: [],
+  imports: [TopicStartComponent],
   templateUrl: './se-theory.component.html',
   styleUrl: './se-theory.component.css'
 })
 export class SeTheoryComponent {
+  isHome: boolean = true
 
+  @HostListener('document:keydown', ['$event'])
+  onDocumentKeydown(event: KeyboardEvent) {
+    this.isHome = false
+  }
 }
