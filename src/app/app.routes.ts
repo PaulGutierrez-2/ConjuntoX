e96f0d1201e58b06e008953caa38632dc25aa51e
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-14 01:10:22 -0500

    Nuevas Rutas y Nuevo Componente "Pantalla de Inicio del tema"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,13 @@ import { RegisterComponent } from './auth/register/register.component';
 import { InfoComponent } from './pages/info/info.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LearningPathComponent } from './pages/learning-path/learning-path.component';
+import { SeTheoryComponent } from './pages/topics/se-theory/se-theory.component';
+import { CardinalityComponent } from './pages/topics/cardinality/cardinality.component';
+import { UnionComponent } from './pages/topics/union/union.component';
+import { IntersectionComponent } from './pages/topics/intersection/intersection.component';
+import { DifferenceComponent } from './pages/topics/difference/difference.component';
+import { ComplementComponent } from './pages/topics/complement/complement.component';
+import { SymmetricDifferenceComponent } from './pages/topics/symmetric-difference/symmetric-difference.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
@@ -11,4 +18,11 @@ export const routes: Routes = [
     {path:'register', component: RegisterComponent},
     {path:'info', component: InfoComponent},
     {path:'learningpath', component: LearningPathComponent},
+    {path:'learningpath/set-theory', component: SeTheoryComponent},
+    {path:'learningpath/cardinality', component: CardinalityComponent},
+    {path:'learningpath/union', component: UnionComponent},
+    {path:'learningpath/intersection', component: IntersectionComponent},
+    {path:'learningpath/difference', component: DifferenceComponent},
+    {path:'learningpath/complement', component: ComplementComponent},
+    {path:'learningpath/symmetric-difference', component: SymmetricDifferenceComponent},
 ];
