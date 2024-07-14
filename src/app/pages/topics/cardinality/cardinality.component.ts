d47d56d450a0cd25c64df073a0c68bbd0d04e71f
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-14 01:12:53 -0500

    Se creo la pagina Cardinalidad

--- src/app/pages/topics/cardinality/cardinality.component.ts
+++ src/app/pages/topics/cardinality/cardinality.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { TopicStartComponent } from '../../../components/topic-start/topic-start.component';
 
 @Component({
   selector: 'app-cardinality',
   standalone: true,
-  imports: [],
+  imports: [TopicStartComponent],
   templateUrl: './cardinality.component.html',
   styleUrl: './cardinality.component.css'
 })
 export class CardinalityComponent {
+  isHome: boolean = true
 
+  @HostListener('document:keydown', ['$event'])
+  onDocumentKeydown(event: KeyboardEvent) {
+    this.isHome = false
+  }
 }
