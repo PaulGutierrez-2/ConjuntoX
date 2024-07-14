30d41f7b20a27f69bdebdc6b31d07d63b591cda7
Paul Andres G. <pagutierrez.2@sudamericano.edu.ec>
2024-07-14 01:16:13 -0500

    Se creo la pagina Interseccion html, css, ts

--- src/app/pages/topics/intersection/intersection.component.ts
+++ src/app/pages/topics/intersection/intersection.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { TopicStartComponent } from '../../../components/topic-start/topic-start.component';
 
 @Component({
   selector: 'app-intersection',
   standalone: true,
-  imports: [],
+  imports: [TopicStartComponent],
   templateUrl: './intersection.component.html',
   styleUrl: './intersection.component.css'
 })
 export class IntersectionComponent {
+  isHome: boolean = true
 
+  @HostListener('document:keydown', ['$event'])
+  onDocumentKeydown(event: KeyboardEvent) {
+    this.isHome = false
+  }
 }
